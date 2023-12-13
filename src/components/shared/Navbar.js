e6a49cc0d31ab1c72892b083c4fa692fa23a4e32
mrokonuzzaman040 <19201103040@cse.bubt.edu.bd>
2023-12-14 01:27:50 +0600

    Add logo and social media icons to Navbar component

--- src/components/shared/Navbar.js
+++ src/components/shared/Navbar.js
@@ -4,28 +4,79 @@ import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Container from '@mui/material/Container';
 import Button from '@mui/material/Button';
+import Image from 'next/image';
 
+// Logo
+import logo from '../../assets/logo.svg'
+import { IconButton } from '@mui/material';
+import { Facebook, GitHub, LinkedIn, Twitter } from '@mui/icons-material';
+import Link from 'next/link';
 
-const pages = ['Products', 'Pricing', 'Blog'];
+const pages = [
+    {
+        route: 'Home',
+        pathname: '/'
+    },
+    {
+        route: 'News',
+        pathname: '/news'
+    },
+    {
+        route: 'categories',
+        pathname: '/categories'
+    },
+    {
+        route: 'About',
+        pathname: '/about'
+    },
+    {
+        route: 'Services',
+        pathname: '/services'
+    },
+    {
+        route: 'Contact',
+        pathname: '/contact'
+    }
+];
 
 function Navbar() {
     return (
         <AppBar position="static">
             <Container maxWidth="xl">
                 <Toolbar disableGutters>
-                    <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
-                        {pages.map((page) => (
-                            <Button
-                                key={page}
-                                sx={{ my: 2, color: 'white', display: 'block' }}
-                            >
-                                {page}
-                            </Button>
+
+                    <Image src={logo} alt="logo" width={150} height={50} />
+
+                    <Box className="text-white flex items-center justify-center" sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+                        {pages.map((item) => (
+                            <Link key={item} href={item.pathname}>
+                                <Button color="inherit">{item.route}</Button>
+                            </Link>
                         ))}
                     </Box>
+
+                    <Box className="flex justify-between">
+                        <IconButton>
+                            <Twitter />
+                        </IconButton>
+
+                        <IconButton>
+                            <GitHub />
+                        </IconButton>
+
+                        <IconButton>
+                            <LinkedIn />
+                        </IconButton>
+
+                        <IconButton>
+                            <Facebook />
+                        </IconButton>
+
+                    </Box>
+
                 </Toolbar>
             </Container>
-        </AppBar>
+        </AppBar >
     );
 }
 export default Navbar;
